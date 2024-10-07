6e1c6dcfcfead8779c18cd767ee9d4572986fe58
KoLili12 <nickzhirnoff@gmail.com>
2024-10-07 10:01:53 +0300

    Initial Commit

---
+++ WATCHIT?Tests/WATCHIT_Tests.swift
@@ -0,0 +1,17 @@
+//
+//  WATCHIT_Tests.swift
+//  WATCHIT?Tests
+//
+//  Created by Николай Жирнов on 07.10.2024.
+//
+
+import Testing
+@testable import WATCHIT_
+
+struct WATCHIT_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
