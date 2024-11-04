779a475351c027063864857b8762f167afe1aee9
KoLili12 <nickzhirnoff@gmail.com>
2024-11-05 00:04:07 +0300

    1

--- WATCHIT?/ViewController.swift
+++ WATCHIT?/ViewController.swift
@@ -8,10 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var MovieButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        MovieButton.layer.cornerRadius = 20
     }
 
 
